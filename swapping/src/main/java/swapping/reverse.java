7aec8d344774a54578af196d4aaddbe3be48e5a2
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-06 14:10:24 +0530

    updated task1

---
+++ swapping/src/main/java/swapping/reverse.java
@@ -0,0 +1,33 @@
+package swapping;
+
+	import java.util.Arrays;
+
+	public class reverse{
+
+	    public static void main(String[] args) {
+
+	        int[] array = {4, 2, 10, 7, 5, 3, 8};
+
+	        Arrays.sort(array);
+
+	        System.out.println("Sorted (ascending): " + Arrays.toString(array));
+
+	        int start = 0;
+
+	        int end = array.length - 1;
+
+	        while (start < end) {
+	            int temp = array[start];
+
+	            array[start] = array[end];
+
+	            array[end] = temp;
+	            start++;
+
+	            end--;
+          System.out.println("Sorted then reversed: " + Arrays.toString(array));
+
+	    }}}
+
+	
+
